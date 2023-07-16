97d127aa9c9da0b8c91330760f2b90e79235409c
Danilo Matos <danilo.dejesusmatos@gmail.com>
2023-07-16 03:57:45 -0300

    Changing App

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('INVOICE_GEN');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('INVOICE_GEN app is running!');
-  });
+  
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Invoice } from './classes/invoice.class';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'INVOICE_GEN';
+  invoice: Invoice = new Invoice();
 }
