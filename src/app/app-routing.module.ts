1a7dfd5fdf118bbed043fe471dbccfad321a0f93
Danilo Matos <danilo.dejesusmatos@gmail.com>
2023-08-09 18:33:40 -0300

    add paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { ConsultantComponent } from './consultant/consultant.component';
 
 
 const routes: Routes = [
 
+  {
+    path: "",
+    component: ConsultantComponent,
+    data: { title: "Consultant" },
+  },
+  {
+    path: "consultant",
+    component: ConsultantComponent,
+    data: { title: "Consultant" },
+  },
   {
     path: "client",
     component: ClientComponent,
