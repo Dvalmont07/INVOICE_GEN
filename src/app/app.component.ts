d337f98ee72f7302ade9a797cb9b913369dc3edd
Danilo Matos <danilo.dejesusmatos@gmail.com>
2024-01-07 22:43:08 -0300

    mudando mes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Invoice } from './classes/invoice.class';
 // import { INVOICE } from './data/public/p-invoice.data';
- import { INVOICE } from './data/private/private-jflimpeza/invoice.data';
-//import { INVOICE } from './data/private/private-eslimpeza/invoice.data';
+//  import { INVOICE } from './data/private/private-jflimpeza/invoice.data';
+import { INVOICE } from './data/private/private-eslimpeza/invoice.data';
 //import { INVOICE } from './data/private/private-artbrilho/invoice.data';
 
 @Component({
