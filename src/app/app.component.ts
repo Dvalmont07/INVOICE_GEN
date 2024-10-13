ed9fa02b359a6f59ec142add9d898965ce90db9f
Danilo Matos <danilo.dejesusmatos@gmail.com>
2024-10-13 20:26:29 -0300

    feat: update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Invoice } from './classes/invoice.class';
 // import { INVOICE } from './data/public/p-invoice.data';
-//import { INVOICE } from './data/private/private-jflimpeza/invoice.data';
+// import { INVOICE } from './data/private/private-jflimpeza/invoice.data';
 //import { INVOICE } from './data/private/private-eslimpeza/invoice.data';
 import { INVOICE } from './data/private/private-artbrilho/invoice.data';
 
